c219cbd47c52c47fb649ddbb088113bb9d7335d5
NghiNguyen <nghinguyenzero@gmail.com>
2024-04-11 14:37:42 +0700

    nav icons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
 import '@/styles/globals.css'
+import { Head } from 'next/head'
+import { Montserrat } from 'next/font/google'
+import Navbar from '@/components/Navbar'
 
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: ['--font-mont']
+
+})
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Navbar/>
+  {/* <Head>
+    <meta name='viewport' content='width=device-width'/>
+    <link rel='icon' href='/favicon.ico'/>
+  </Head> */}
+
+    <main className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}>
+      <Component {...pageProps} />
+    </main>
+  </>
+  )
 }
+
